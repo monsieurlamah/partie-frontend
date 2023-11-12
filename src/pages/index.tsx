bebfa7b367d1b8e94318248cc78c6f214b658c91
monsieurlamah <monsieurlamah2024@gmail.com>
2023-11-12 15:03:47 +0000

    frontend

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Seo from '@/ui/components/seo/seo'
+
+
+
+export default function Home() {
+  return (  
+    <>
+    <Seo title='Monsieur Lamah' description='Description...'/>
+    <h1>Hello world</h1>
+    </>
+  
+  )
+}
